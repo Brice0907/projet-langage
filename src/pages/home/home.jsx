7d55869f695349c2ed295be6388b5008c79a316d
Rochet brice <brice.974452@gmail.com>
2023-04-25 15:55:48 +0200

    Page de présentation avec 2-3 anim/transition

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,17 @@
-import "./style/style.scss"
-
+import "./style/style.scss";
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
-    return <div>
-        <h2 className="test">Bienvenue !</h2>
-        <div className="test">Bienvenue sur The Code Exploreur, le lieu idéal pour les amateurs de code qui cherchent à tester leurs connaissances.
-            Avec une vaste sélection de questions sur des langages populaires, vous pouvez tester vos compétences et apprendre de nouvelles choses en même temps.
-            Le site est conçu pour tout type de personnes, offrant une variété de difficultés pour tous les niveaux
+    return <div className="bloc">
+        <h2 className="bloc_title">Bienvenue !</h2>
+        <div className="bloc_text">Bienvenue sur The Code Exploreur, le lieu idéal pour les amateurs de code qui cherchent à tester leurs connaissances.
+            Avec une vaste sélection de questions sur des langages populaires, vous pouvez tester vos compétences et apprendre de nouvelles choses en même temps. <br></br>
+            Le site est conçu pour tout type de personnes, offrant une variété de difficultés pour tous les niveaux.
+        </div>
+        <div className="bloc_btn">
+            <NavLink to='' className='bloc_btn_left bloc_btn_style'>Accéder au site</NavLink>
+            <NavLink to='' className='bloc_btn_mid bloc_btn_style'>Compte</NavLink>
+            <NavLink to='' className='bloc_btn_right bloc_btn_style'>Documentation</NavLink>
         </div>
     </div>
 }
