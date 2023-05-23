7bed9e681a4a3cda6238c23299a30c5983317a8f
Rochet brice <brice.974452@gmail.com>
2023-05-23 15:48:40 +0200

    début du code du hover des difficultés

--- src/pages/question/question.jsx
+++ src/pages/question/question.jsx
@@ -6,15 +6,22 @@ export default function Question() {
 
     const [levelIndex, setLevelIndex] = useState('')
 
+    const [montre, setMontre] = useState(false);
+
     if (levelIndex === "") {
         return <div className='difficulte'>
             <Nav />
             <div className='difficulte_bloc'>
                 <h2 className='difficulte_bloc_title'>Choisissez votre difficulté :</h2>
                 <div className='display'>
-                    <div onClick={() => setLevelIndex("easy")} className='difficulte_bloc_btn difficulte_bloc_btn_easy'>Facile</div>
+                    <div onClick={() => setLevelIndex("easy")} onMouseEnter={() => setMontre(true)} onMouseLeave={() => setMontre(false)} className='difficulte_bloc_btn difficulte_bloc_btn_easy test'>Facile</div>
                     <div onClick={() => setLevelIndex('medium')} className="difficulte_bloc_btn difficulte_bloc_btn_medium">Moyen</div>
                     <div onClick={() => setLevelIndex('hard')} className="difficulte_bloc_btn difficulte_bloc_btn_hard">Difficile</div>
+                </div>
+                <div className='difficulte_bloc_description'>
+                    <div className={montre ? 'content visible' : 'content difficulte_bloc_description_texte difficulte_bloc_description_texte_easy'}>le texte facile</div>
+                    <div className='difficulte_bloc_description_texte difficulte_bloc_description_texte_medium'>le texte medium</div>
+                    <div className='difficulte_bloc_description_texte difficulte_bloc_description_texte_hard'>le texte hard</div>
                 </div>
                 {/* Rajouter du texte en mode facile c'est des question très simple .... medium pour les personnes ayant deja un peu codé avec .. et hard pour les experts qui souhaite se challender */}
             </div>
@@ -24,3 +31,4 @@ export default function Question() {
     }
 
 }
+// className='difficulte_bloc_description_texte difficulte_bloc_description_texte_easy'
