ff69f1a22ffcb5ee17f98005a61802f4e20b4312
Rochet brice <brice.974452@gmail.com>
2023-05-31 16:58:46 +0200

    affichage des questions, plus lire commentaire

--- src/components/easy/easy.jsx
+++ src/components/easy/easy.jsx
@@ -8,8 +8,23 @@ export default function easy({ question }) {
     let numerotage = 0
     console.log(question[id]);
 
+    let score = 0
+    // faire un usestate pour quand la reponse est selectionner
+    // faire un if/else pour la selection de la question, si deja selectionner on deselectionne sinon on selectionne en ajoutant un class css pour voir le truc séléctionné
+    // faire une fonction pour le bouton suivant/valider qui vérifie si la reponse séléctionner a la valeur true en faisant un find()
+    // si la reponse est true on ajoute 1 au score et on passe a la question suivant, sans oublier de mettre le usestate de sélection a zero
+
     return <div>
         <div>Question {question[id][numerotage].id} : </div>
+        <div>{question[id][numerotage].question}</div>
+
+        <div>
+            {question[id][numerotage].reponse.map((reponse, index) => (
+                <div key={index}>
+                    {reponse.texte}
+                </div>
+            ))}
+        </div>
     </div>
 }
 
